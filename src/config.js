70c1394a7d9b79589e6d5de0797e9a3b10023864
Nykollas <nicolauarti09@gmail.com>
2021-02-19 00:23:46 -0300

    config setted

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
     ? 'http://localhost:8080'
-    : 'https://iot-flix.herokuapp.com';
+    : 'https://topicmaif.herokuapp.com';
 
 export default {
     URL_BACKEND,
